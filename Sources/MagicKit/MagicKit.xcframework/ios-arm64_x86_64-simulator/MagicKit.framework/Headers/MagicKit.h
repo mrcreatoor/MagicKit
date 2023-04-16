b1da4c6d013fa7ccde430b46eff1beec05a928aa
MrCreator <abdo.devios@gmail.com>
2023-04-16 15:02:51 +0000

    Add xcframework

---
+++ Sources/MagicKit/MagicKit.xcframework/ios-arm64_x86_64-simulator/MagicKit.framework/Headers/MagicKit.h
@@ -0,0 +1,18 @@
+//
+//  MagicKit.h
+//  MagicKit
+//
+//  Created by Abdellatif Abaamrane on 4/15/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MagicKit.
+FOUNDATION_EXPORT double MagicKitVersionNumber;
+
+//! Project version string for MagicKit.
+FOUNDATION_EXPORT const unsigned char MagicKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MagicKit/PublicHeader.h>
+
+
